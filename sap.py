888fa164fcd7ab3ea94987315802800cab69150c
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-03-03 23:50:04 +0000

    Minor changes

--- sap.py
+++ sap.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Requests job site
 website_url = "https://jobs.sap.com/go/SAP-Jobs-in-Ireland/851301/?q=&q2=&alertId=&locationsearch=&title=&location=Dublin+IE"
 res = requests.get(website_url)
 res.raise_for_status()
 
+# Parse data
 soup = BeautifulSoup(res.text, "html.parser")
 job_titles = soup.find_all("span", class_="jobTitle visible-phone")
 internship_titles = []
@@ -21,12 +23,14 @@ for link in job_titles:
         star_titles.append(job_text)
 
 print('\n')
+
 # Intern jobs
 print(f"There are {len(internship_titles)} internship titles:")
 for job in internship_titles:
     print(job)
 
 print('\n')
+
 # STAR JOBS
 print(f"There are {len(star_titles)} star titles:")
 for job in star_titles:
